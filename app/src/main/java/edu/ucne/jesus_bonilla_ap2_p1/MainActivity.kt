100ce5e5e657263ac692e92c229f83609c5a2748
JesusBonilla9 <jesus_bonilla@ucne.edu.do>
2026-06-03 21:19:47 -0400

    Modifica MainActivity.kt

--- app/src/main/java/edu/ucne/jesus_bonilla_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/jesus_bonilla_ap2_p1/MainActivity.kt
@@ -4,6 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.jesus_bonilla_ap2_p1.presentation.navigate.RegistroNavHost
 import edu.ucne.jesus_bonilla_ap2_p1.ui.theme.Jesus_Bonilla_AP2_P1Theme
 
 class MainActivity : ComponentActivity() {
@@ -12,8 +20,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Jesus_Bonilla_AP2_P1Theme {
-
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    RegistroNavHost(innerPadding = innerPadding)
+                }
             }
         }
     }
 }
+
+
